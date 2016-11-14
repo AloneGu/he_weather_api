e477d4bd7ddd2d96cac328e836f9d4fab0f649c0
Jackling Gu <wg@gizwits.com>
2016-11-14 11:34:39 +0800

    Updated

---
+++ config_example.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: Jackling Gu
+@file: config_example.py
+@time: 16-11-14 11:21
+"""
+
+API_KEY = 'xxxxx'
---
+++ weather_worker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@author: Jackling Gu
+@file: weather_worker.py
+@time: 16-11-14 11:20
+"""
+
+class WeatherWorker(object):
+
+    def __init__(self):
+        pass
